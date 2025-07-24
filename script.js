57d473d636eba4eaa6606501832275a7546949e7
nandinidas07 <nandinidas107@gmail.com>
2025-07-24 21:46:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+
+document.getElementById("votingForm").addEventListener("submit", function (e) {
+  e.preventDefault(); 
+
+  const age = document.getElementById("age").value.trim();
+  const name = document.getElementById("name").value.trim();
+
+  if (!age || !name) {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (parseInt(age) > 18) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 4000);
+  })
+    .then(() => {
+      alert(Welcome, ${name}. You can vote.);
+    })
+    .catch(() => {
+      alert(Oh sorry ${name}. You aren't old enough.);
+    });
+});
